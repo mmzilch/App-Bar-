e39b13da5c9a6cf6d20dcf1de3e62ef28f915a06
mmzilch <xing12138>
2020-07-24 08:54:37 +0630

    first commit

---
+++ app/src/main/java/com/example/appbartest/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.appbartest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        collapsingToolbar.title = "This is title"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+        return if(id == R.id.menu_smile){
+            true
+        }else super.onOptionsItemSelected(item)
+
+    }
+}
